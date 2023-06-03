5d2e0fd3c9d82e9206af2675f5a1d9dfe1e612ac
wkzhang <8808994+dazaiqianyuan@user.noreply.gitee.com>
2023-06-03 10:18:47 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('demo1', { title: 'Express' });
 });
-router.get('/login2', function(req, res, next) {
-  res.render('login2', { title: 'Express' });
+router.get('/demo2', function(req, res, next) {
+  res.render('demo2', { title: 'Express' });
 });
-// router.get('/login2', function(req, res, next) {
-//   res.render('login', { title: 'Express' });
-// });
+router.get('/demo3', function(req, res, next) {
+  res.render('demo3', { title: 'Express' });
+});
+router.get('/demo4', function(req, res, next) {
+  res.render('demo4', { title: 'Express' });
+});
+router.get('/demo5', function(req, res, next) {
+  res.render('demo5', { title: 'Express' });
+});
 module.exports = router;
