970ff61393dfba0b90153deb6af66c5d7bc97c98
wkzhang <8808994+dazaiqianyuan@user.noreply.gitee.com>
2023-06-13 08:19:28 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('demo1', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('demo4', { title: 'Express' });
+// });
+router.get('/demo1', function(req, res, next) {
+  res.render('demo4', { title: 'Express' });
 });
 router.get('/demo2', function(req, res, next) {
   res.render('demo2', { title: 'Express' });
@@ -11,10 +14,10 @@ router.get('/demo2', function(req, res, next) {
 router.get('/demo3', function(req, res, next) {
   res.render('demo3', { title: 'Express' });
 });
-router.get('/demo4', function(req, res, next) {
-  res.render('demo4', { title: 'Express' });
-});
-router.get('/demo5', function(req, res, next) {
-  res.render('demo5', { title: 'Express' });
-});
+// router.get('/demo4', function(req, res, next) {
+//   res.render('demo1', { title: 'Express' });
+// });
+// router.get('/demo5', function(req, res, next) {
+//   res.render('demo5', { title: 'Express' });
+// });
 module.exports = router;
